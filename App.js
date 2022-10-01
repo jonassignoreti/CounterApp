a9dd4a1d23a37fc4ce57733b7c45eb712328a969
jonassignoreti <jonassignoreti7@gmail.com>
2022-10-01 11:03:04 -0300

    creating some design

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, View, Appearance, useColorScheme } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [ number, setNumber ] = useState(0)
-
+  
   const inc = () => setNumber(number + 1)
   const dec = () => setNumber(number - 1)
+  const reset = () => {
+    setNumber(0)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar />
       <Text style={styles.text}>{number}</Text>
-      <Button title='+' color="#fff" onPress={inc}/>
-      <Button title='-' color="#f194ff" onPress={dec}/>
+      <View style={styles.button}>
+        <Button title='+' onPress={inc}/>
+      </View>
+      <View style={styles.button}>
+        <Button title='-' onPress={dec}/>
+      </View>
+      <View style={styles.button}>
+        <Button title='RESET' onPress={reset}/>
+      </View>
     </SafeAreaView>
   );
 }
@@ -21,16 +31,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
+    // backgroundColor: (cs === 'dark') ? '#000' : '#fff' ,
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
-    fontSize: 36
+    color: '#888888',
+    textAlign: 'center',
+    fontSize: 128
   },
   button: {
-    backgroundColor: '#fff',
-
+    margin: 10,
+    padding: 40,
+    backgroundColor: '#dddddd',
+    borderRadius: 10
   }
 });
